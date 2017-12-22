f06df2fb8beeb3674ddb6f35bfe62d0129396428
Maxim Zhemerenko <maxim.zhemerenko@outlook.com>
2017-12-22 21:44:12 +0200

    Console project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppLocalizationUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
