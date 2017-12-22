e29a000c4724b3810788073fa1d7cb07c44afd79
Maxim Zhemerenko <maxim.zhemerenko@outlook.com>
2017-12-22 22:48:29 +0200

    Refactoring

--- Program.cs
+++ Program.cs
@@ -16,7 +16,11 @@ namespace AppLocalizationUtil
         {
             using (var client = new HttpClient())
             {
-                using (var result = await client.GetStreamAsync("https://onedrive.live.com/download?resid=C62576FDA2F2250C!1418&ithint=file%2cxlsx&app=Excel&wdo=2&authkey=!AHpmF6oJPcGoeSw"))
+                var resId = "C62576FDA2F2250C!1418";
+                var authKey = "!AOiLnVvvWMZtbv4";
+                var url = $"https://onedrive.live.com/download?resid={resId}&authkey={authKey}";
+                
+                using (var result = await client.GetStreamAsync(url))
                 {
                     using(var file = new FileStream(Environment.CurrentDirectory + "/wrk.xlsx", FileMode.Create)) {
                         await result.CopyToAsync(file);
