1973bddba5accd7c623ea116acb88c4cb09f7491
maxim <maxim.zhemerenko@spryrocks.com>
2017-12-25 12:41:09 +0200

    Config file completed

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace AppLocalizationUtil
         static async Task Run()
         {
             var resId = "C62576FDA2F2250C!1418";
-            var authKey = "";
+            var authKey = "!AD1pPffa7vMvUEA";
             var fileName = Environment.CurrentDirectory + "/wrk.xlsx";
 
             IFileDownloader fileDownloader = new OneDriveFileDownloader(resId, authKey, fileName);
