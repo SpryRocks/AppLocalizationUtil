5ee1d5b79f900b69bd8d869a65ca5bfbbc6eea13
maximzhemerenko <maxim.zhemerenko@spryrocks.com>
2019-09-30 13:08:08 +0300

    Add empty line

--- Data/Loaders/ExcelFileDocumentReader.cs
+++ Data/Loaders/ExcelFileDocumentReader.cs
@@ -57,97 +57,11 @@ namespace AppLocalizationUtil.Data.Loaders
 
             for (int i = 0; i < numberOfSheets; i++)
             {
-                List<LocalizationItem> items = new List<LocalizationItem>();
-
                 var sheet = workbook.GetSheetAt(i);
 
-                var firstRow = sheet.GetRow(sheet.FirstRowNum);
-                if (firstRow == null) 
+                var items = ReadSheet(sheet);
+                if (items == null) 
                     continue;
-
-                IDictionary<int, Language> col_lng = new Dictionary<int, Language>();
-                IDictionary<int, string> col_key = new Dictionary<int, string>();
-                int? col_apps = null;
-
-                foreach (var cell in firstRow.Cells)
-                {
-                    var value = cell.StringCellValue;
-                    var columnIndex = cell.ColumnIndex;
-                    
-                    if (_configuration.LanguageColumns.ContainsKey(value))
-                    {
-                        col_lng.Add(columnIndex, _configuration.LanguageColumns[value]);
-                        continue;
-                    }
-
-                    if (_configuration.PlatformKeyColumns.ContainsKey(value))
-                    {
-                        col_key.Add(columnIndex, _configuration.PlatformKeyColumns[value]);
-                        continue;
-                    }
-
-                    if (_configuration.AppsColumn == value)
-                    {
-                        col_apps = columnIndex;
-                        continue;
-                    }
-                }
-
-                for (int rowNum = firstRow.RowNum + 1; rowNum <= sheet.LastRowNum; rowNum++)
-                {
-                    var row = sheet.GetRow(rowNum);
-                    if (row == null) 
-                        break;
-
-                    IDictionary<Language, string> values = new Dictionary<Language, string>();
-                    IDictionary<string, string> keys = new Dictionary<string, string>();
-                    ISet<string> apps = null;
-
-                    foreach (var cell in row.Cells)
-                    {
-                        var value = cell.StringCellValue;
-                        if (string.IsNullOrWhiteSpace(value))
-                            value = null;
-
-                        if (value == null)
-                            continue;
-
-                        int columnIndex = cell.ColumnIndex;
-                        
-                        if (col_lng.ContainsKey(columnIndex))
-                        {
-                            var language = col_lng[columnIndex];
-
-                            values.Add(language, value);
-                            
-                            continue;
-                        }
-
-                        if (col_key.ContainsKey(columnIndex))
-                        {
-                            var key = col_key[columnIndex];
-
-                            keys.Add(key, value);
-
-                            continue;
-                        }
-
-                        if (col_apps == columnIndex)
-                        {
-                             apps = value.Split(new[] {",", ";"}, StringSplitOptions.RemoveEmptyEntries).Select(it => it.Trim()).ToHashSet();
-
-                             continue;
-                        }
-                    }
-
-                    if (values.Count < 1)
-                        continue;
-
-                    if (apps == null)
-                        apps = new HashSet<string>();
-
-                    items.Add(new LocalizationItem { Values = values, Keys = keys, Apps = apps });
-                }
 
                 groups.Add(new Group { Name = sheet.SheetName, Items = items });
             }
@@ -158,6 +72,102 @@ namespace AppLocalizationUtil.Data.Loaders
                 Languages = _configuration.LanguageColumns.Values.ToHashSet(),
                 Platforms = _configuration.PlatformKeyColumns.Values.ToHashSet()
             };
+        }
+
+        private List<LocalizationItem> ReadSheet(ISheet sheet)
+        {
+            var items = new List<LocalizationItem>();
+
+            var firstRow = sheet.GetRow(sheet.FirstRowNum);
+            if (firstRow == null)
+                return null;
+
+            IDictionary<int, Language> col_lng = new Dictionary<int, Language>();
+            IDictionary<int, string> col_key = new Dictionary<int, string>();
+            int? col_apps = null;
+
+            foreach (var cell in firstRow.Cells)
+            {
+                var value = cell.StringCellValue;
+                var columnIndex = cell.ColumnIndex;
+
+                if (_configuration.LanguageColumns.ContainsKey(value))
+                {
+                    col_lng.Add(columnIndex, _configuration.LanguageColumns[value]);
+                    continue;
+                }
+
+                if (_configuration.PlatformKeyColumns.ContainsKey(value))
+                {
+                    col_key.Add(columnIndex, _configuration.PlatformKeyColumns[value]);
+                    continue;
+                }
+
+                if (_configuration.AppsColumn == value)
+                {
+                    col_apps = columnIndex;
+                    continue;
+                }
+            }
+
+            for (int rowNum = firstRow.RowNum + 1; rowNum <= sheet.LastRowNum; rowNum++)
+            {
+                var row = sheet.GetRow(rowNum);
+                if (row == null)
+                    break;
+
+                IDictionary<Language, string> values = new Dictionary<Language, string>();
+                IDictionary<string, string> keys = new Dictionary<string, string>();
+                ISet<string> apps = null;
+
+                foreach (var cell in row.Cells)
+                {
+                    var value = cell.StringCellValue;
+                    if (string.IsNullOrWhiteSpace(value))
+                        value = null;
+
+                    if (value == null)
+                        continue;
+
+                    int columnIndex = cell.ColumnIndex;
+
+                    if (col_lng.ContainsKey(columnIndex))
+                    {
+                        var language = col_lng[columnIndex];
+
+                        values.Add(language, value);
+
+                        continue;
+                    }
+
+                    if (col_key.ContainsKey(columnIndex))
+                    {
+                        var key = col_key[columnIndex];
+
+                        keys.Add(key, value);
+
+                        continue;
+                    }
+
+                    if (col_apps == columnIndex)
+                    {
+                        apps = value.Split(new[] {",", ";"}, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(it => it.Trim()).ToHashSet();
+
+                        continue;
+                    }
+                }
+
+                if (values.Count < 1)
+                    continue;
+
+                if (apps == null)
+                    apps = new HashSet<string>();
+
+                items.Add(new LocalizationItem {Values = values, Keys = keys, Apps = apps});
+            }
+
+            return items;
         }
 
         private void BugWorkarround_DeleteTmpFiles() 
